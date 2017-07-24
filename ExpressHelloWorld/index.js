5e301c9b21846caf4a4a11a913f494a31f437d90
biberius <biberius@gmail.com>
2017-07-24 12:17:01 +0300

    Express backend

--- ExpressHelloWorld/index.js
+++ ExpressHelloWorld/index.js
@@ -5,6 +5,10 @@ app.get('/', function (req, res) {
   res.send('Hello World');
 });
  
+app.post('/', function (req, res) {
+  res.send('No POST available');
+});
+ 
 app.listen(3000);
 
 console.log("Listening for requests on http://127.0.0.1:3000");
